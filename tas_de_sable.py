f80fc6ca55d2d352977ed240028eec4185ea418f
uvsq22101593 <91332757+uvsq22101593@users.noreply.github.com>
2022-02-11 12:20:18 +0100

    Update tas_de_sable.py

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -16,15 +16,6 @@ CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
 canvas = tk.Canvas(racine, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
 canvas.grid(column=0, row=1)
 
-#constantes
-
-# hauteur du canvevas
-HAUTEUR = 500
-# largeur du canevas
-LARGEUR = 500
-# taille de la grille
-N = 10
-
 
 
 #création des widgets
