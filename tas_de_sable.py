7f380e75b84e96c3b991c518647151c67d8f1021
uvsq22101593 <91332757+uvsq22101593@users.noreply.github.com>
2022-02-11 12:18:36 +0100

    projet

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -9,4 +9,26 @@
 
 import tkinter as tk
 
+racine =tk.Tk()
+racine.title("Tas de sable")
 
+CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
+canvas = tk.Canvas(racine, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+canvas.grid(column=0, row=1)
+
+#constantes
+
+# hauteur du canvevas
+HAUTEUR = 500
+# largeur du canevas
+LARGEUR = 500
+# taille de la grille
+N = 10
+
+
+
+#création des widgets
+bouton= tk.Button(racine, text="Configuration courante", bg ="white")
+bouton.grid(column=0, row=2)
+
+racine.mainloop()
