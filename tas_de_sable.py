86fb33ee300751cf1eb44248082454be2c8c1c1a
Tilly SEASSAU <you@example.com>
2022-02-12 15:33:28 +0100

    j'ai écrit la fonction qui initialise la configuration courante

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -23,6 +23,30 @@ canvas.grid(column=0, row=1)
 bouton = tk.Button(racine, text="Configuration courante", bg ="white")
 bouton.grid(column=0, row=2)
 
-
+#fonction qui initialise la configuration courante
+def configurationCourante() : 
+  taillecarre = 10
+  couleur0 = "white"
+  couleur1 = "black"
+  couleur2 = "blue"
+  couleur3 = "purple"
+  
+  for y in range(25): #creation des cases 
+    for x in range(25):
+        x1 = x*taillecarre 
+        y1 = y*taillecarre
+        x2 = x1 + taillecarre
+        y2 = y1 + taillecarre
+        couleur = random.randint(0,3) #determination de la couleur de chaque case
+        if couleur == 0: 
+          couleur = couleur0
+        elif couleur == 1 : 
+          couleur = couleur1
+        elif couleur == 2 : 
+          couleur = couleur2
+        elif couleur == 3 : 
+          couleur= couleur3
+        canvas.create_rectangle((x1, y1, x2, y2), fill=couleur)
+  return 
 
 racine.mainloop()
