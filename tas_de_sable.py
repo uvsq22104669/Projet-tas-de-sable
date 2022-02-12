9ea4689153b70ba92ad114333d5c72cb0b4ecf3b
Tilly SEASSAU <you@example.com>
2022-02-12 16:29:04 +0100

    première fonction qui crée la configuration courante a été écrite

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -17,20 +17,9 @@ CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
 canvas = tk.Canvas(racine, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
 canvas.grid(column=0, row=1)
 
-
-
-#création des widgets
-bouton = tk.Button(racine, text="Configuration courante", bg ="white")
-bouton.grid(column=0, row=2)
-
 #fonction qui initialise la configuration courante
 def configurationCourante() : 
   taillecarre = 10
-  couleur0 = "white"
-  couleur1 = "black"
-  couleur2 = "blue"
-  couleur3 = "purple"
-  
   for y in range(25): #creation des cases 
     for x in range(25):
         x1 = x*taillecarre 
@@ -39,15 +28,19 @@ def configurationCourante() :
         y2 = y1 + taillecarre
         couleur = random.randint(0,3) #determination de la couleur de chaque case
         if couleur == 0: 
-          couleur = couleur0
+          couleur = "white"
         elif couleur == 1 : 
-          couleur = couleur1
+          couleur = "black"
         elif couleur == 2 : 
-          couleur = couleur2
+          couleur = "blue"
         elif couleur == 3 : 
-          couleur= couleur3
+          couleur= "purple"
         canvas.create_rectangle((x1, y1, x2, y2), fill=couleur)
-  print("Une case blanche signifie qu'il y a un aucun grain de sable, une case noire 1 grain, une case bleue 2 grains et une case violete 3 grains")
+    print("Une case blanche signifie qu'il y a un aucun grain de sable, une case noire 1 grain, une case bleue 2 grains et une case violete 3 grains")
   return 
+
+#création des widgets
+bouton = tk.Button(racine, text="Configuration courante", bg ="white", command = configurationCourante)
+bouton.grid(column=0, row=2)
 
 racine.mainloop()
