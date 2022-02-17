57720030e8bbe87bf96af32fbe769a18b50873ea
Tilly SEASSAU <you@example.com>
2022-02-17 22:09:38 +0000

    Merge branch 'main' of https://github.com/uvsq22104669/Projet-tas-de-sable into main

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -16,17 +16,26 @@ racine.title("Tas de sable")
 CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
 canvas = tk.Canvas(racine, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
 canvas.grid(column=0, row=1)
+# creation de la matrice qui va servir pour la configuration courante (on peut l'inserer dans la fonction si besoin)
+dimension = 5
+matrice = []
+for j in range(dimension) : 
+  listej = []
+  for i in range(dimension) : 
+    listej.append(random.randint(0,3))
+  matrice.append(listej)
+print(np.array(matrice))
 
 #fonction qui initialise la configuration courante
 def configurationCourante() : 
   taillecarre = 10
-  for y in range(25): #creation des cases 
-    for x in range(25):
+  for y in range(dimension): #creation des cases 
+    for x in range(dimension):
         x1 = x*taillecarre 
         y1 = y*taillecarre
         x2 = x1 + taillecarre
         y2 = y1 + taillecarre
-        couleur = random.randint(0,3) #determination de la couleur de chaque case
+        couleur = matrice[y][x] #determination de la couleur de chaque case
         if couleur == 0: 
           couleur = "white"
         elif couleur == 1 : 
@@ -36,7 +45,6 @@ def configurationCourante() :
         elif couleur == 3 : 
           couleur= "purple"
         canvas.create_rectangle((x1, y1, x2, y2), fill=couleur)
-    print("Une case blanche signifie qu'il y a un aucun grain de sable, une case noire 1 grain, une case bleue 2 grains et une case violete 3 grains")
   return 
 
 #création des widgets
