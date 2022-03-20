e00709e9b894a64bb02a9d2084e23eb48cb565f2
uvsq22101593 <91332757+uvsq22101593@users.noreply.github.com>
2022-03-20 16:58:08 +0100

    Update tas_de_sable.py

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,4 +1,3 @@
-#####
 # Groupe  LDDBI 5
 # Laurédane COUSTILLAS
 # Alexandre SCHOUMSKY
@@ -7,27 +6,37 @@
 #https://github.com/uvsq22104669/Projet-tas-de-sable
 #######
 
+# Import des librairies
 import tkinter as tk
 import random
+import numpy as np
 
 racine = tk.Tk()
 racine.title("Tas de sable")
 
+# Définition des constantes
 CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
+NB_CASES_GRILLE = 50
+
 canvas = tk.Canvas(racine, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
 canvas.grid(column=0, row=1)
+
 # creation de la matrice qui va servir pour la configuration courante (on peut l'inserer dans la fonction si besoin)
-dimension = 5
+
+dimension = 50
 matrice = []
 for j in range(dimension) : 
   listej = []
   for i in range(dimension) : 
     listej.append(random.randint(0,3))
   matrice.append(listej)
-print(np.array(matrice))
+  print(np.array(matrice))
 
 #fonction qui initialise la configuration courante
 def configurationCourante() : 
+  """Fonction qui associe chaque case à un nombre de grains de sable aléatoire entre 0 et 3 inclus ainsi qu'une couleur selon le 
+  nombre de grains de sable"""
+  global configurationCourante
   taillecarre = 10
   for y in range(dimension): #creation des cases 
     for x in range(dimension):
@@ -35,7 +44,8 @@ def configurationCourante() :
         y1 = y*taillecarre
         x2 = x1 + taillecarre
         y2 = y1 + taillecarre
-        couleur = matrice[y][x] #determination de la couleur de chaque case
+
+        couleur = matrice[y][x] #détermination de la couleur de chaque case
         if couleur == 0: 
           couleur = "white"
         elif couleur == 1 : 
@@ -51,4 +61,4 @@ def configurationCourante() :
 bouton = tk.Button(racine, text="Configuration courante", bg ="white", command = configurationCourante)
 bouton.grid(column=0, row=2)
 
-racine.mainloop()
+racine.mainloop()   
